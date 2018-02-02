68f01198808c4c9488d12a4d03d26892b82810e1
idrys <idrys2@gmail.com>
2018-02-02 01:10:15 +0100

    Notifications

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Notifications\NewUpdetesCaddecor;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,6 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
+    $user->notify(new NewUpdetesCaddecor($invoice));
     return $request->user();
 });
